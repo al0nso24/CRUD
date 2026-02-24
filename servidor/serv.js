1819e11f24300aad25ea3623d6bc195083bd040e
Alonso Zegarra Velasquez <zegarrav24@gmail.com>
2026-02-24 13:03:02 -0500

    Primer commit

---
+++ servidor/serv.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "alonso_24122005_",
+    database: "empleados"
+})
+
+//Agregar un nuevo empleado:
+app.post("/nuevoEmpleado", (req, res)=>{
+    const {nombre, edad, pais, cargo, sueldo} = req.body;
+    db.query("INSERT INTO emp(nombre, edad, pais, cargo, sueldo) VALUES(?,?,?,?,?)",
+        [nombre, edad, pais, cargo, sueldo], (err, result)=>{
+        if(err){
+            console.log("error");
+        }else{
+            res.send(result);
+        }
+    })
+})
+
+//Lista de empleados:
+app.get("/listarEmpleados", (req, res)=>{
+    db.query("SELECT id, nombre, edad, pais, cargo, sueldo, (sueldo * 0.18) AS igv FROM emp", 
+        (err, result)=>{
+            if(err){
+                console.log("error");
+            }else{
+                res.send(result);
+            }
+        }
+    )
+})
+
+//Borrar empleado:
+app.delete("/borrarEmpleado/:id", (req, res)=>{
+    const id = req.params.id;
+    db.query("DELETE FROM emp WHERE id=?", [id], (err, result)=>{
+        if(err){
+            console.log("error");
+        }else{
+            res.send(result);
+        }
+    })
+})
+
+//Actualizar información del empleado:
+app.put("/actualizar/:id", (req, res)=>{
+    const id = req.params.id;
+    const {nombre, edad, pais, cargo, sueldo} = req.body;
+    db.query("UPDATE emp SET nombre=?, edad=?, pais=?, cargo=?, sueldo=? WHERE id=?", 
+        [nombre, edad, pais, cargo, sueldo, id], (err, result)=>{
+            if(err){
+                console.log("error");
+            }else{
+                res.send(result);
+            }
+        }
+    )
+})
+
+//Este es mi puerto:
+app.listen(3001, ()=>{
+    console.log("Puerto activado.")
+})
